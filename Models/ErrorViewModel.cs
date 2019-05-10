1008a8d054c47fa742e135dd98912f3b8b21579f
Emanuel Paul <emanuel.paul@hotmail.com>
2019-05-10 20:04:56 +0300

    added mvc project, added to gitignore wwwroot/lib

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp_net_core_mailgun_httpclient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
